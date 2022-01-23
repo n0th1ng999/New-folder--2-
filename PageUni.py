fc1cb0c39f6e50f8ad1b5a44a0c36455e702b16a
n0th1ng999 <tiagogabrielpereira@gmail.com>
2022-01-23 20:43:53 +0000

    Tá mais ou menos

--- PageUni.py
+++ PageUni.py
@@ -1,12 +1,10 @@
 from fileinput import filename
-from importlib.metadata import files
+
 import os
-from re import T
 from tkinter import Button, Toplevel
 from unicodedata import name
 
 def callback(name):
-    name = Faculdades['text']
     fileName = "Uni" + name
     os.system('python '+fileName+'.py')
     print(name)
@@ -18,17 +16,17 @@ def callback(name):
 
 totalDir = 0
 paginaBlah = Toplevel()
-pageUni = r"infoProjects"
+pageUni = r"infoCursos"
 for base, dirs, files in os.walk(pageUni):
     for directories in dirs:
         totalDir += 1
         naoSei = dirs[totalDir-1]
-        Faculdades = Button(paginaBlah,text = naoSei, relief = "flat",font = 20,command =lambda: callback(naoSei))
+        Faculdades = Button(paginaBlah,text = naoSei, relief = "flat",font = 20,command = lambda naoSei=naoSei: callback(naoSei))
         Faculdades.pack()
         print(dirs[totalDir-1])
         
        
 
-print(f"o total blahfsuh e {totalDir}")
+print("o total blahfsuh e {totalDir}")
 
 paginaBlah.mainloop()
